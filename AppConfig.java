3651997a868ccdeb9cfa703287a30af739eb8889
omaima ben hamouda <oumaimabenhamouda18@gmail.com>
2023-10-29 12:29:28 +0100

    enviroment viarables added

---
+++ AppConfig.java
@@ -0,0 +1,32 @@
+package encyptiondecryptionproject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+class AppConfig{
+
+    private static final Properties enVars=new Properties();
+    static { loadEnVars();}
+
+    public static void loadEnVars(){
+
+        try{
+            FileInputStream fileInputStream=new FileInputStream("paths.env");
+            enVars.load(fileInputStream);
+            fileInputStream.close();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getEncryptionDirectory(){
+        return enVars.getProperty("ENCRYPTION_FOLDER");
+    }
+    public static String getDecryptionDirectory(){
+        return enVars.getProperty("DECRYPTION_FOLDER");
+    }
+
+
+}
